24191a51c297bfd9549b8515fe637c3c9818386a
gurungtara02 <53060644+gurungtara02@users.noreply.github.com>
2020-04-07 13:39:14 -0500

    first commit comment

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello world ! from github.com..");
+        System.out.print("Sandesh Lamsal");
+
+        int[] intArray = {100,200,300};
+        for (int a:intArray){
+            System.out.println(a);
+        }
+	//TarA
+    }
+}
